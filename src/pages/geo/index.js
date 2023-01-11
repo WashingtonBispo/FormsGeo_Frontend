6d24a9d4b0ec6e3b24e4e08a3dd09c4a70385bde
marcelog5 <marcelovictorg2@gmail.com>
2023-01-11 11:07:10 -0300

    Added multiple points

--- src/pages/geo/index.js
+++ src/pages/geo/index.js
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, useMapEvents, Circle } from 'react-leaflet'
 
 import {
   Box,
@@ -8,7 +8,20 @@ import {
   useColorModeValue,
   Text,
   Switch,
-  Button
+  Button,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
 } from '@chakra-ui/react';
 
 import { api } from '../../services/api';
@@ -27,7 +40,13 @@ import {
 
 const Geo = () => {
   const [initialPosition, setInitialPosition] = useState(null);
+  const [positions, setPositions] = useState([]);
   const [selectedPosition, setSelectedPosition] = useState([0,0]);
+  const [selectedRadio, setSelectedRadio] = useState(0);
+  const [selectedFormFinalGetGeo, setSelectedFormFinalGetGeo] = useState(false);
+  const [selectedAsyncGetGeo, setSelectedAsyncGetGeo] = useState(false);
+
+  const { isOpen: isOpenShareResearch, onOpen: onOpenShareResearch, onClose: onCloseShareResearch } = useDisclosure();
 
   const color = useColorModeValue('white', 'gray.700');
 
@@ -55,27 +74,28 @@ const Geo = () => {
   }, [showErrorToast]);
 
   const Markers = () => {
-
     const map = useMapEvents({
         click(e) {                                
             setSelectedPosition([
                 e.latlng.lat,
                 e.latlng.lng
-            ]);                
+            ]); 
+            
+            onOpenShareResearch();
         },            
-    })
+    })    
+  };
 
-    return (
-        selectedPosition ? 
-            <Marker           
-            key={selectedPosition[0]}
-            position={selectedPosition}
-            interactive={false} 
-            />
-        : null
-    )   
-    
-}
+  const handleSaveSelectedPosition = () => {
+    const position = [selectedPosition[0], selectedPosition[1], selectedRadio];
+
+    let tempPositions = positions.map(p => p);
+    tempPositions.push(position);
+
+    setPositions(tempPositions);
+
+    onCloseShareResearch();
+  };
 
   const renderMap = useCallback(() => {
     return (
@@ -90,9 +110,22 @@ const Geo = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {positions && positions.map((position => {
+          return (
+            <>
+              <Marker           
+                key={selectedPosition[0]}
+                position={[position[0], position[1]]}
+                interactive={false} 
+              />
+
+              <Circle center={[position[0], position[1]]} radius={position[2]} />
+            </>
+          );
+        }))}
       </MapContainer>
     );
-  }, [selectedPosition, initialPosition]);
+  }, [selectedPosition, initialPosition, positions]);
 
   return (
     <>
@@ -137,7 +170,7 @@ const Geo = () => {
                   </Text>
 
                   <SwitchContainer>
-                    <Switch size="md" colorScheme='green' />
+                    <Switch size="md" colorScheme='green' onChange={e => console.log(e.target.value)}/>
 
                     <Text fontSize='14px'>
                       Gravar na finalização do formulário pelo participante.
@@ -176,6 +209,87 @@ const Geo = () => {
           </Box>
         </ResearchsContainer>
       </BodyContainer>
+
+      <Modal blockScrollOnMount={false} isOpen={isOpenShareResearch} onClose={onCloseShareResearch}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            <Text 
+              fontSize='16px' 
+              color='#3F4254'
+            >
+              Ajuste o raio que será utilizado para coletar os dados geográficos!
+            </Text>
+          </ModalHeader>
+
+          <ModalCloseButton />
+
+          <ModalBody
+            display='flex'
+            justifyContent='center'
+          >
+            <Text 
+              fontSize='15px' 
+              color='#3F4254'
+              width='100%'
+              border='2px dotted #E4E6EF'
+              padding='4px'
+              textAlign='center'
+            >
+              x:{selectedPosition[0]}
+            </Text>
+
+            <Text 
+              fontSize='15px' 
+              color='#3F4254'
+              width='100%'
+              border='2px dotted #E4E6EF'
+              padding='4px'
+              textAlign='center'
+            >
+              y:{selectedPosition[1]}
+            </Text>
+
+            <NumberInput 
+              onChange={e => setSelectedRadio(e)}
+              defaultValue={10}
+              max={200}
+              min={5}
+              clampValueOnBlur={false}
+              >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </ModalBody>
+
+          <ModalFooter 
+            display='flex'
+            justifyContent='space-between'
+          >
+            <Button 
+              backgroundColor={'#F5F8FA'}
+              color={'#7E8299'}
+              mr={3}
+              onClick={onCloseShareResearch}
+            >
+              Voltar
+            </Button>
+
+            <Button 
+              backgroundColor={'#00A3FF'}
+              color={'white'}
+              mr={3}
+              onClick={handleSaveSelectedPosition}
+            >
+              Adicionar
+            </Button>
+            
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </> 
   );
 };
